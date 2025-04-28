05e3bef05b71509c25b02657afa96212a12d2593
Omkar-Bhosale-912 <omkarbhosale912@gmail.com>
2025-04-28 11:40:04 +0530

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,29 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './Components/Navbar';
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#34C94B",
+      },
+      secondary: {
+        main: "#121212",
+      },
+      typography: {
+        fontFamily: `'Poppins', sans-serif`, 
+      },
+    },
+  });
 
   return (
-    <>
-     
-    </>
+    <ThemeProvider theme={theme}>
+    <Navbar />
+    </ThemeProvider>
   )
 }
 
