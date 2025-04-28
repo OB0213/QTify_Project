4b28f9594f458ecc4db67e90bbf4d67430cedbf3
Omkar-Bhosale-912 <omkarbhosale912@gmail.com>
2025-04-28 12:26:00 +0530

    Fourth Commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,27 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './Components/Navbar';
 import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 
 function App() {
   const [count, setCount] = useState(0);
   const theme = createTheme({
     palette: {
+      typography: {
+        fontFamily: "Poppins, sans-serif", // 👈 Important: no extra quotes!
+      },
       primary: {
         main: "#34C94B",
       },
       secondary: {
         main: "#121212",
       },
-      typography: {
-        fontFamily: `'Poppins', sans-serif`, 
-      },
     },
   });
 
   return (
     <ThemeProvider theme={theme}>
+      <CssBaseline />
     <Navbar />
     </ThemeProvider>
   )
